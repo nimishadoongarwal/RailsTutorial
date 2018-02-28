af4725783e43327c3b2cd85974153ac7e3b22c1b
Nimisha Doongarwal <ndoongarwal@zendesk.com>
2018-02-28 10:29:11 -0800

    Ruby Rails tutorial app

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController <ApplicationController #inherit from base controller
+
+  def home #action for the controller
+  end
+
+  def about #action for the controller
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyApp::Application.config.secret_token = '2c39305a2a5fa8e8fd854f67b1025bdab672da6a1223aee1e6cc50b6722290f519e8837d3ee96685daa841e5ea066f2e51901297880f8153985759f3849e84e9'
